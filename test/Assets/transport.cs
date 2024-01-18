e7bca3d9489eda5337936750d3f5d20820d8064b
huajijun <zzy1427947151@gmail.com>
2024-01-18 10:43:20 +0800

    for test

---
+++ test/Assets/transport.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class transport : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // void Start()
+    // {   // 加载新的场景  索引 或者名字
+    //     //SceneManager.LoadScene(1);
+    //     // 获取当前场景
+    //     Scene  scene = SceneManager.GetActiveScene();
+    //     // 获取名字
+    //     Debug.Log(SceneManager.GetActiveScene().name);
+    //     // 场景是否被加载了
+    //     Debug.Log(scene.isLoaded);
+    //     // 场景路径
+    //     Debug.Log(scene.path);
+    //     // 场景的索引
+    //     Debug.Log(scene.buildIndex);
+
+    //     GameObject[] gos = scene.GetRootGameObjects();
+    //     Debug.Log(gos);
+
+    //     // 场景管理类
+    //     Debug.Log(SceneManager.sceneCount);
+    //     Scene script_scene = SceneManager.CreateScene("script scene");
+    //     SceneManager.UnloadSceneAsync(script_scene);
+
+    //     // SceneManager.LoadScene(1,LoadSceneMode.Single);
+    //     // 叠加
+    //     SceneManager.LoadScene(1,LoadSceneMode.Additive);
+
+    // }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
